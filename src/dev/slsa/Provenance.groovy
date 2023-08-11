6af8b9f61e6e7ab2dbb049ac331dca293d357601
Angel Campos <63554840+angelcamposm@users.noreply.github.com>
2023-08-11 13:05:15 +0200

    Create Provenance.groovy

---
+++ src/dev/slsa/Provenance.groovy
@@ -0,0 +1,62 @@
+package dev.slsa
+
+def LinkedHashMap provenance = [:]
+
+def Void construct() {
+    
+    def LinkedHashMap payload = [:]
+
+    payload.put('buildDefinition', [:])
+    payload.put('runDetails', [:])
+    
+    payload.buildDefinition.put('buildType', 'https://www.jenkins.io/Pipeline')
+    payload.buildDefinition.put('externalParameters', [:])
+    payload.buildDefinition.put('internalParameters', [:])
+    payload.buildDefinition.put('resolvedDependencies', [])
+    
+    payload.runDetails.put('builder', [:])
+    payload.runDetails.put('metadata', [:])
+    payload.runDetails.put('byproducts', [])
+    
+    payload.runDetails.builder.put('id', getBuilderId())
+    payload.runDetails.builder.put('builderDependencies', [])
+    payload.runDetails.builder.put('version', '')
+    
+    payload.runDetails.metadata.put('invocationId', 0)
+    payload.runDetails.metadata.put(startedOn, getStartTimestamp())
+    payload.runDetails.metadata.put('finishedOn', '')
+    
+    provenance = payload
+}
+
+def String getBuilderId() {
+    return "${env.BUILD_URL}"
+}
+
+def String getStartTimestamp() {
+    def String startTime = new Date(currentBuild.startTimeInMillis).format("yyyy-MM-dd'T'h:m:ss.SSS")
+    return "${startTime}Z"
+}
+
+
+
+
+
+
+
+def Void print() {
+    println(JsonOutput.prettyPrint(toJson())
+}
+
+def String toJson() {
+    return JsonOutput.toJson(provenance)
+}
+
+def Void write(Boolean pretty = true) {
+    
+    def String payload = pretty 
+        ? JsonOutput.prettyPrint(toJson())
+        : toJson()
+
+    writeFile encoding: 'UTF-8', file: 'provenance.json', text: payload
+}
