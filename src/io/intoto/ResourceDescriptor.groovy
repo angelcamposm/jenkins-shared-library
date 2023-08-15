20bbed25283a67589130e6d523ba34abec2e0b60
Angel Campos Muñoz <angel.campos.m@outlook.com>
2023-08-15 18:57:26 +0200

    wip: add ResourceDescriptor class

--- src/io/intoto/ResourceDescriptor.groovy
+++ src/io/intoto/ResourceDescriptor.groovy
@@ -2,99 +2,72 @@ package io.intoto
 
 import groovy.json.JsonOutput
 
-/*
- | ----------------------------------------------------------------------------
- | Resource Descriptor
- | ----------------------------------------------------------------------------
- | A size-efficient description of any software artifact or resource (mutable 
- | or immutable).
- |
- | For more information, please visit: 
- | - https://github.com/in-toto/attestation/blob/main/spec/v1/resource_descriptor.md
- */
+class ResourceDescriptor {
 
-def LinkedHashMap rsd = [:]
+    def LinkedHashMap payload = [:]
 
-def String resourceName = ''
+    ResourceDescriptor() {
 
-def Void file(String fileName) {
-
-    resourceName = fileName
-
-    def LinkedHashMap resourceDescriptor = [:]
-
-    resourceDescriptor.put('name', fileName)
-    resourceDescriptor.put('size', getFileSize())
-    resourceDescriptor.put('digest', getDigestSet())
-
-    if (sh(script: 'which file || echo "NotFound"', returnStdout: true).trim().toString() == 'NotFound') {
-        println('`file` command not found. Skipping mediaType check.')
-    } else {
-        resourceDescriptor.put('mediaType', getMediaType())
-    }
-    
-    resourceDescriptor.put('createdAt', getTimestamp())
-
-    rsd = resourceDescriptor
-}
-
-def LinkedHashMap getDigestSet() {
-
-    def DigestSet ds = new io.intoto.DigestSet()
-
-    ds.construct(resourceName)
-
-    return ds.get()
-}
-
-def Void setDigestSet(DigestSet ds) {
-    rsd.digest = ds.get()
-}
-
-def Integer getFileSize() {
-    return sh(
-        label: 'Get file size in bytes.',
-        script: "stat --format=%s ${resourceName}",
-        returnStdout: true
-    ).trim().toInteger()
-}
-
-def String getMediaType() {
-    return sh(
-        label: 'Get MIME type.',
-        script: "file -b --mime-type ${resourceName}",
-        returnStdout: true,
-        encoding: 'UTF-8'
-    ).trim().toString()
-}
-
-def String getTimestamp() {
-
-    def Long timestamp = sh(
-        label: 'Get file Birth date.',
-        script: "stat --format=%W ${resourceName}",
-        returnStdout: true
-    ).trim().toLong()
-    
-    if (timestamp == 0) {
-        timestamp = sh(
-            label: 'Get file Access date.',
-            script: "stat --format=%X ${resourceName}",
-            returnStdout: true
-        ).trim().toLong()
     }
 
-    return Timestamp.fromSeconds(timestamp)
-}
+    def ResourceDescriptor addAnnotation(String key, value) {
+        this.assign('annotations', [:])
+        this.annotations.put(key, value)
+        return this
+    }
 
-def LinkedHashMap get() {
-    return rsd
-}
+    def ResourceDescriptors annotations(LinkedHashMap annotations) {
+        this.assign('annotations', annotations)
+        return this
+    }
+    
+    def ResourceDescriptor content(String content) {
+        this.assign('content', content)
+        return this
+    }
 
-def Void print() {
-    println(JsonOutput.prettyPrint(toJson()))
-}
+    def ResourceDescriptor digest(LinkedHashMap digestSet) {
+        this.assign('digest', digestSet)
+        return this
+    }
+    
+    def ResourceDescriptor downloadLocation(String location) {
+        this.assign('downloadLocation', location)
+        return this
+    }
 
-def String toJson() {
-    return JsonOutput.toJson(rsd)
+    def ResourceDescriptor mediaType(String type) {
+        this.assign('mediaType', type)
+        return this
+    }
+
+    def ResourceDescriptor name(String name) {
+        this.assign('name', name)
+        return this
+    }
+
+    def ResourceDescriptor uri(String uri) {
+        this.assign('uri', uri)
+        return this
+    }
+
+    def Void assign(String key, value) {
+        if (this.payload.containsKey(key)) {
+            this.payload."$key" = value
+        } else {
+            this.payload.put(key, value)
+        }
+    }
+    
+    def LinkedHashMap get() {
+        return payload
+    }
+
+    def Void print() {
+        println(JsonOutput.prettyPrint(this.toJson()))
+    }
+
+    def String toJson() {
+        return JsonOutput.toJson(payload)
+    }
 }
