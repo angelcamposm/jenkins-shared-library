a5f8d44b413bf4ca78600bf0d0318641528f087b
Angel Campos Muñoz <angel.campos.m@outlook.com>
2023-08-16 02:17:59 +0200

    wip: refactor ResourceDescriptor

--- src/io/intoto/ResourceDescriptor.groovy
+++ src/io/intoto/ResourceDescriptor.groovy
@@ -22,52 +22,46 @@ class ResourceDescriptor {
     }
 
     def ResourceDescriptor addAnnotation(String key, value) {
-        this.assign('annotations', [:])
+        if (!this.payload.containsKey('annotations')) {
+            this.payload.put('annotations', [:])
+        }
         this.payload.annotations.put(key, value)
         return this
     }
 
     def ResourceDescriptor annotations(LinkedHashMap annotations) {
-        this.assign('annotations', annotations)
+        this.payload.put('annotations', annotations)
         return this
     }
     
     def ResourceDescriptor content(String content) {
-        this.assign('content', content)
+        this.payload.put('content', content)
         return this
     }
 
     def ResourceDescriptor digest(LinkedHashMap digestSet) {
-        this.assign('digest', digestSet)
+        this.payload.put('digest', digestSet)
         return this
     }
     
     def ResourceDescriptor downloadLocation(String location) {
-        this.assign('downloadLocation', location)
+        this.payload.put('downloadLocation', location)
         return this
     }
 
     def ResourceDescriptor mediaType(String type) {
-        this.assign('mediaType', type)
+        this.payload.put('mediaType', type)
         return this
     }
 
     def ResourceDescriptor name(String name) {
-        this.assign('name', name)
+        this.payload.put('name', name)
         return this
     }
 
     def ResourceDescriptor uri(String uri) {
-        this.assign('uri', uri)
+        this.payload.put('uri', uri)
         return this
-    }
-
-    def Void assign(String key, value) {
-        if (this.payload.containsKey(key) && key != 'annotations') {
-            this.payload."$key" = value
-        } else {
-            this.payload.put(key, value)
-        }
     }
     
     def LinkedHashMap get() {
