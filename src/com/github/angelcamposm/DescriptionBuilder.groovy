cd5072b34b4d12fd01a52143bc98e3d340445b16
Angel Campos Muñoz <angel.campos.m@outlook.com>
2025-10-06 21:42:12 +0200

    add DescriptionBuilder

--- src/com/github/angelcamposm/DescriptionBuilder.groovy
+++ src/com/github/angelcamposm/DescriptionBuilder.groovy
@@ -73,6 +73,7 @@ class DescriptionBuilder {
 
         return licenses.collect { license ->
             if (license instanceof String) {
+                return '<a class="jenkins-table__link" href="https://opensource.org/licenses/' + name + ' " target="_blank">' + name + '</a>'
                 return license // Handles simple string from package.json or composer.json
             }
             if (license instanceof Map) {
